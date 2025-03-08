2cd8b5239dd4949bf0ec916e0536d4d05b653e78
katheland <katheland@gmail.com>
2025-03-07 23:29:29 -0500

    map and mapb

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"strings"
 	"bufio"
 	"os"
+	"net/http"
+	"io"
+	"encoding/json"
 )
 
 // The structure of a valid command
@@ -11,23 +14,49 @@ type cliCommand struct {
 	name string
 	description string
 	callback func() error
+	config *Config
+}
+
+type Config struct {
+	next string
+	previous *string
 }
 
 // A map of valid commands that the program recognizes, and what to do with them
 var validCommands map[string]cliCommand
+//var mapConfig Config
 
 // but it's important to initialize the map here, to avoid an initialization loop
 func init() {
+	mapConfig := Config{
+		next: "https://pokeapi.co/api/v2/location-area/",
+		previous: nil,
+	}
+
 	validCommands = map[string]cliCommand{
 		"exit": {
 			name: "exit",
 			description: "Exit the Pokedex",
 			callback: commandExit,
+			config: nil,
 		},
 		"help": {
 			name: "help",
 			description: "Displays a help message",
 			callback: commandHelp,
+			config: nil,
+		},
+		"map": {
+			name: "map",
+			description: "Lists areas",
+			callback: commandMap,
+			config: &mapConfig,
+		},
+		"mapb": {
+			name: "mapb",
+			description: "Lists areas",
+			callback: commandMapB,
+			config: &mapConfig,
 		},
 	}
 }
@@ -42,7 +71,10 @@ func main() {
 		//fmt.Println("Your command was:", input[0])
 		command, exists := validCommands[input[0]]
 		if exists {
-			command.callback()
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
 		} else {
 			fmt.Println("Unknown command")
 		}
@@ -69,4 +101,57 @@ func commandHelp() error {
 		fmt.Println(fmt.Sprintf("%s: %s", val.name, val.description))
 	}
 	return nil
+}
+
+type Location struct {
+	Count    int    `json:"count"`
+	Next     string `json:"next"`
+	Previous *string    `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
+
+// print a page of 20 locations
+func mapFunction(url *string) error {
+	if url == nil {
+		fmt.Println("you're on the first page")
+		return nil
+	}
+
+	res, err := http.Get(*url)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	jsonData, err := io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+
+	var locations Location
+	if err = json.Unmarshal(jsonData, &locations); err != nil {
+		return err
+	}
+
+	for _, loc := range locations.Results {
+		fmt.Println(loc.Name)
+	}
+
+	validCommands["map"].config.next = locations.Next
+	validCommands["map"].config.previous = locations.Previous
+	
+	return nil
+}
+
+// print the next page of locations
+func commandMap() error {
+	return mapFunction(&validCommands["map"].config.next)
+}
+
+// print the previous page of locations
+func commandMapB() error {
+	return mapFunction(validCommands["map"].config.previous)
 }
